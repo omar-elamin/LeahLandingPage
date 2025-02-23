6df67a9adc043431f4229e04a43c04daf0d77b4b
Omar Elamin <elmon11bussiness@gmail.com>
2025-02-23 02:14:18 +0100

    lint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type React from "react"
 import "./globals.css"
-import { Inter } from "next/font/google"
+// import { Inter } from "next/font/google"
 
 // const inter = Inter({ subsets: ["latin"] })
 
